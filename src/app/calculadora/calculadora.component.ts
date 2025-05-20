408efea4e1e3f450ed54b4e6d8bb1adc0321aa1e
Adriano Melo <140020059+oAdrino@users.noreply.github.com>
2025-05-20 17:18:23 -0300

    algumas mudanças

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calculadora',
-  imports: [],
+  imports: [ FormsModule ],
   templateUrl: './calculadora.component.html',
   styleUrl: './calculadora.component.scss'
 })
@@ -14,6 +15,8 @@ export class CalculadoraComponent {
   calcularResultado(){
 
     console.log("Chamando o método para calcular o resultado!");
+    console.log('Numero1: ' + this.numero1)
+    console.log('Numero2: ' + this.numero2)
     this.resultado = this.numero1 + this.numero2;
   }
-}s
+}

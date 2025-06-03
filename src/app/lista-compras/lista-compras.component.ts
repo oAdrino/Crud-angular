7773b72f88a95ba16e77773e0925f5a8db94baf2
Adriano Melo <140020059+oAdrino@users.noreply.github.com>
2025-06-03 17:08:18 -0300

    Sistema de adicionar itens na propria planilha

--- src/app/lista-compras/lista-compras.component.ts
+++ src/app/lista-compras/lista-compras.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule} from '@angular/forms'
+import { ItemLista } from './itemlista'
 @Component({
   selector: 'app-lista-compras',
   imports: [ FormsModule ],
@@ -10,9 +11,17 @@ export class ListaComprasComponent {
 
 
   item: string = '';
+  lista: ItemLista[] = [] 
+
 
   adicionarItem(){
-    console.log('Item recebido: ', this.item);
+    let itemlista = new ItemLista();
+    itemlista.nome = this.item;
+    itemlista.id = this.lista.length + 1;
+
+    this.lista.push(itemlista);
+
+
     this.item = '';
   }
 }
